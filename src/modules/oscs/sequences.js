bfafb453ffe25fc415d46f3d4f70684a8a327846
Servio Palacios <servio@palacios.com>
2019-11-15 17:29:08 -0500

    [deleted unused code]

--- src/modules/oscs/sequences.js
+++ src/modules/oscs/sequences.js
@@ -68,6 +68,7 @@ function buildFetchRequest({ state }) {
   return { requests };
 }
 
+
 export const fetch = sequence("oscs.fetch", [
   ({ state, props }) => ({
 		connection_id: state.get("oscs.connection_id"),
