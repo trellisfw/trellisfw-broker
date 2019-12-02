0bff142dd1f5591eac1e2e68b8903be49d01b5fe
Servio Palacios <servio@palacios.com>
2019-12-02 16:44:11 -0500

    [v0.5.7 fixes #2] - [complete set of OSC's templates added + script]

--- src/modules/demo/sequences.js
+++ src/modules/demo/sequences.js
@@ -4,8 +4,10 @@ import { set } from "cerebral/operators";
 import { state } from "cerebral/tags";
 import oada from "@oada/cerebral-module/sequences";
 import { pac_dataset } from "../../components/offline_datasets.js";
+import { osc_dataset } from "../../components/offline_datasets.js";
 
 let _localPACSPath = "/bookmarks/pacs";
+let _localOSCSPath = "/bookmarks/oscs";
 
 let tree = {
   bookmarks: {
@@ -22,15 +24,37 @@ let tree = {
   }
 };
 
+let OSCtree = {
+  bookmarks: {
+    _type: "application/vnd.oada.bookmarks.1+json",
+    _rev: "0-0",
+    oscs: {
+      _type: "application/vnd.oada.yield.1+json",
+      _rev: "0-0",
+      "*": {
+        _type: "application/vnd.oada.yield.1+json",
+        _rev: "0-0"
+      }
+    }
+  }
+};
+
 const upload_demo_pacs = [
   createPACS,
 	createPACRequest,
 	oada.put
 ];
 
+const upload_demo_oscs = [
+  createOSCS,
+	createOSCRequest,
+	oada.put
+];
+
 export const init = sequence("demo.init", [
 	set(state`demo.loading`, true),
 	upload_demo_pacs,
+	upload_demo_oscs,
 	set(state`demo.loading`, false),
 ]);
 
@@ -57,6 +81,40 @@ function createPACRequest({ props, state }) {
 			data:          pac,
 			path:          `${_localPACSPath}/${pac.id}`,
 			tree: tree
+		};
+		requests.push(request);
+	}//for
+ 
+	return {
+    connection_id: connection_id,
+    requests:      requests,
+    domain:        state.get("oada_domain")
+  };
+}
+
+function createOSCS({ props, state }) {
+	let oscs = [];
+  let keys = Object.keys(osc_dataset.records);
+	
+	for (let key of keys) {
+    let osc = osc_dataset.records[key];
+    oscs.push(osc);
+	}
+
+	return { oscs: oscs };
+}
+
+function createOSCRequest({ props, state }) {
+  let connection_id = state.get("oscs.connection_id");
+  let requests = [];
+
+	for (let osc of props.oscs) {
+		console.log(osc.id);
+		let request = {
+			connection_id: connection_id,
+			data:          osc,
+			path:          `${_localOSCSPath}/${osc.id}`,
+			tree: OSCtree
 		};
 		requests.push(request);
 	}//for
